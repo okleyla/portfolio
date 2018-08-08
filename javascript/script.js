91c2c703d1ca416ff7cfcb9121bb27007cedd0f2
okleyla <gokuyan@gmail.com>
2018-08-08 17:20:35 -0400

    updated case study, ready for review

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+//Nav Bar//
 function myFunction() {
     var x = document.getElementById("topnav");
     if (x.className === "nav-bar") {
@@ -9,4 +10,21 @@ function myFunction() {
 
 function closeNav() {
     document.getElementById("topnav").style.width = "0%";
+}
+
+//collapse//
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
 }
